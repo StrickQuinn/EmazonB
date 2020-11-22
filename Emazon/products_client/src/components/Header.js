e321969478fce886a718d84cf12afe9367baa683
masterkong06 <wintonbw@gmail.com>
2020-11-22 14:23:30 -0500

    added css for Header, ProductCart, ProductGrid

--- Emazon/products_client/src/components/Header.js
+++ Emazon/products_client/src/components/Header.js
@@ -1,16 +1,80 @@
 import React, { Component } from "react";
+import ListGroup from 'react-bootstrap/ListGroup';
+import Carousel from 'react-bootstrap/Carousel';
+// import '../index.css'
+
+
 
 export default class Header extends Component {
   render() {
     return (
+
+      
+
       <div className="navbarTop">
         <div className="ScreenTop"> </div>
         <div className="Banner">
-          {" "}
-          E-Mazon <br />
-          <span ClassName="logo">Logo</span>
+        <ListGroup horizontal className="list">
+         
+         <ListGroup.Item>Gift Cards</ListGroup.Item>
+         <ListGroup.Item> Get Ultra</ListGroup.Item>
+         <ListGroup.Item>Find a Gift</ListGroup.Item>
+         <ListGroup.Item>Special Sales</ListGroup.Item>
+         <ListGroup.Item>We are Hiring </ListGroup.Item>
+       </ListGroup>
+
+
+    
+       <Carousel className="carousel">
+  <Carousel.Item >
+    <img
+      className="img-responsive"
+      src="https://images.pexels.com/photos/257897/pexels-photo-257897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" 
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item >
+    <img
+      className="img-responsive"
+      src="https://imgur.com/undefined"
+      alt="Third slide"
+      bsPrefix
+    />
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <img
+      className="img"
+      src="https://imgur.com/undefined"
+      alt="Third slide"
+    />
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel> 
+
+     
+            <h1 >E-Mazon</h1>
+           <br />
+          <img ClassName="logo"  />
           <i className="fa fa-shopping-cart"></i>
+          <div className="navbar">
+       
+    
+      
+     </div>
+        
         </div>
+
         <div className="SearchBar">
           <form onSubmit={this.handleSubmit}>
             <input type="search" name="search" placeholder="Search.."></input>
@@ -18,16 +82,9 @@ export default class Header extends Component {
             <i type="submit" className="fa fa-search"></i>
           </form>
         </div>
-        <div className="navBar">
-          
-          <ul>
-            {" "}
-            <li> Holiday Deals </li> |<li> Gift Cards </li> |
-            <li> Get Ultra </li> |<li> Find a Gift </li> |
-            <li> Special Sales </li> |<li> We are Hiring </li>
-          </ul>{" "}
-        </div>
+       
       </div>
+    
     );
   }
 }
